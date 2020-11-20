096da3eeb028c83fca117d69b9575033f768c440
otoyo <hiroki.t.1988@gmail.com>
2020-11-20 17:54:48 +0900

    Fix git status and filename

--- git-fzf.zsh
+++ git-fzf.zsh
@@ -1,12 +1,12 @@
 function __git-fzf() {
   local preview='echo {} | \
     awk "
-      \$1==\"A\"   { print \"git diff --color --staged \"\$2 }
-      \$1==\"M\"   { print \"git diff --color \"\$2 }
-      \$1==\"\?\?\"{ print \"echo \\\"Untracked file\\\"\" }" | \
+      \$0 ~ /^[AMD] / { print \"git diff --color --staged \"\$2 }
+      \$0 ~ /^ [AMD]/ { print \"git diff --color \"\$2 }
+      \$0 ~ /^\?\? /  { print \"echo \\\"Untracked file\\\"\" }" | \
     xargs -L1 -I{} zsh -c {}'
 
-  local selected=$(__git-fzf__git_status | fzf -m --ansi --preview="$preview")
+  local selected=$(__git-fzf__git_status | fzf -m --ansi --preview="$preview" | awk '{ print $2 }')
   if [[ -n "$selected" ]]; then
     selected=$(tr '\n' ' ' <<< "$selected")
     echo $selected
@@ -15,11 +15,27 @@ function __git-fzf() {
 
 function __git-fzf__git_status() {
   unbuffer git status -s | \
-    awk '
-      match($1, /\x1b\[[0-9;]*mA/)    { print    substr($0, RSTART, RLENGTH)"\x1b\[0m  "$2 }
-      match($1, /\x1b\[[0-9;]*mM/)    { print " "substr($0, RSTART, RLENGTH)"\x1b\[0m " $2 }
-      match($1, /\x1b\[[0-9;]*m\?\?/) { print    substr($0, RSTART, RLENGTH)"\x1b\[0m " $2 }
-    '
+    awk '{
+      # ?? file
+      if (match($0, /^\x1b\[[0-9;]*m\?\?\x1b\[m/)) {
+        print substr($0, RSTART, RLENGTH)" "$2
+      } else {
+        # MM file
+        if (match($0, /^\x1b\[[0-9;]*m[AMD]\x1b\[m\x1b\[[0-9;]*m[AMD]\x1b\[m/)) {
+          match($1, /^\x1b\[[0-9;]*m[AMD]\x1b\[m/)
+          print substr($0, RSTART, RLENGTH)"  "$2
+
+          match($1, /\x1b\[[0-9;]*m[AMD]\x1b\[m$/)
+          print " "substr($0, RSTART, RLENGTH)" "$2
+        # M  file
+        } else if (match($0, /^\x1b\[[0-9;]*m[AMD]\x1b\[m/)) {
+          print substr($0, RSTART, RLENGTH)"  "$2
+        #  M file
+        } else if (match($0, /^ \x1b\[[0-9;]*m[AMD]\x1b\[m/)) {
+          print substr($0, RSTART, RLENGTH)" "$2
+        }
+      }
+    }'
 }
 
 function git-fzf-widget() {
