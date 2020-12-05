44dbc55d35fecd7ca244d6ad4d8497fc29219d1a
otoyo <hiroki.t.1988@gmail.com>
2020-12-05 11:45:47 +0900

    Allow to select branch in git log

--- fzf-git.zsh
+++ fzf-git.zsh
@@ -14,10 +14,24 @@ function __fzf-git() {
 }
 
 function __fzf-git-commit() {
-  local preview='echo {} | awk "{ print \"git diff --color \"\$1\"~1 \" \$1 }" | xargs -L1 -I{} zsh -c {}'
-  local selected=$(unbuffer git --no-pager log --pretty=format:'%C(auto)%h %ad %an - %s%d' --date=format:'%Y-%m-%d %H:%M:%S' | \
-    fzf -m --ansi --preview="$preview" | \
-    awk '{ print $1 }')
+  local source_git_branch='echo "Press => to select commits" && unbuffer git branch'
+
+  local source_git_log='echo "Press <= to select branch" && \
+    unbuffer git --no-pager log --pretty=format:"%C(auto)%h %ad %an - %s%d" --date=format:"%Y-%m-%d %H:%M:%S"'
+
+  local preview=' \
+    if [[ {} =~ " " ]] ; then \
+      echo {} | awk "{ print \"git diff --color \"\$1\"~1 \" \$1 }" | xargs -L1 -I{} zsh -c {}; \
+    else \
+      '$source_git_log' {1}; \
+    fi'
+
+  local selected=$(zsh -c $source_git_log | \
+    fzf --ansi --multi --reverse --preview "$preview" --header-lines 1 \
+      --bind 'left:reload('$source_git_branch')' \
+      --bind 'right:reload('$source_git_log' {1})' \
+      --bind 'tab:toggle+up' | \
+    sed -e 's/\* //g' | awk '{ print $1 }')
   if [[ -n "$selected" ]]; then
     selected=$(tr '\n' ' ' <<< "$selected")
     echo $selected
